3c5c13e800bc81ca5b181afd3708afcf2927461b
Darya Tugaeva <dasha.tugaeva95@mail.ru>
2024-01-19 18:30:35 +0500

    Добавление сущностей для личного кабинета

---
+++ src/main/java/com/example/psds/personal_account/Repository/GroupRepository.java
@@ -0,0 +1,9 @@
+package com.example.psds.personal_account.Repository;
+
+import com.example.psds.personal_account.model.Group;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupRepository extends JpaRepository<Group, Long> {
+}
+
+
